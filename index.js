1c9abcf1df540eaf3887bb125e6ffde235b0433f
trinhxuankhanh <xuankhanhs@gmail.com>
2020-09-21 16:05:13 +0700

    make server

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
+var port = process.env.YOUR_PORT || process.env.PORT || 8080;
 
 server.use(middlewares);
 server.use(router);
-server.listen((process.env.POST || 8080), () => {
+server.listen(port, () => {
   console.log("JSON Server is running");
 });
